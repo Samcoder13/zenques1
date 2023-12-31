212ef6aba582148a1ba66ede1d2bce2ebdf0045a
Samarth <98737191+Samcoder13@users.noreply.github.com>
2024-01-01 00:43:50 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import ProductList from './ProductList'
+import { useEffect } from 'react'
+// import Second from './Second'
+// import Temp from './Temp'
+// import PropertySelector from './PropertySelector'
+// import FileLoader from './FileLoader'
+// import Third from './Third'
+function App() {
+  const [count, setCount] = useState(0)
+  const [products, setproducts] = useState({})
+  const [sortedproducts, setsortedproducts] = useState([])
+  //
+  useEffect(() => {
+    const fetchdata= async()=>{
+      await fetch('https://s3.amazonaws.com/open-to-cors/assignment.json').then(res=>res.json()).then((data)=>{
+        setproducts(data.products);
+       
+      }).catch((error)=>{
+        console.log(error);
+      })
+  
+      const productsArray = Object.entries(products).map(([id, product]) => ({
+        id,
+        ...product
+      }));
+      const sortedProductsArray = productsArray.sort((a, b) => b.popularity - a.popularity);
+      // Sort the array based on the 'popularity' property
+
+      setsortedproducts(sortedProductsArray);
+      console.log(sortedproducts);
+  
+    }
+    fetchdata();
+
+  }, [sortedproducts])
+  
+  return (
+    <>
+    <h1>Displaying all Products</h1>
+    
+     <ProductList data={sortedproducts}/>
+    </>
+  )
+}
+
+export default App
